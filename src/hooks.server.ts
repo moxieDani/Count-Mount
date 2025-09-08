984cdfc6ed089e0e08c31cc7f59cba285a219002
Kisoon Kwon <moxie2ks@gmail.com>
2025-09-08 13:49:46 +0900

    환경변수명 변경

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -8,8 +8,8 @@ async function refreshAccessToken(token: any) {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: new URLSearchParams({
-        client_id: env.GOOGLE_CLIENT_ID,
-        client_secret: env.GOOGLE_CLIENT_SECRET,
+        client_id: env.VITE_GOOGLE_CLIENT_ID,
+        client_secret: env.VITE_GOOGLE_CLIENT_SECRET,
         grant_type: "refresh_token",
         refresh_token: token.refreshToken,
       }),
@@ -39,8 +39,8 @@ async function refreshAccessToken(token: any) {
 export const { handle } = SvelteKitAuth({
   providers: [
     Google({
-      clientId: env.GOOGLE_CLIENT_ID,
-      clientSecret: env.GOOGLE_CLIENT_SECRET,
+      clientId: env.VITE_GOOGLE_CLIENT_ID,
+      clientSecret: env.VITE_GOOGLE_CLIENT_SECRET,
       authorization: {
         params: {
           scope: 'openid email profile https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/spreadsheets',
--- src/routes/api/config/+server.ts
+++ src/routes/api/config/+server.ts
@@ -4,7 +4,7 @@ import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async () => {
 	return json({
-		googleApiKey: env.GOOGLE_API_KEY,
-		googleClientId: env.GOOGLE_CLIENT_ID
+		googleApiKey: env.VITE_GOOGLE_API_KEY,
+		googleClientId: env.VITE_GOOGLE_CLIENT_ID
 	});
 };
